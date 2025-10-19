6904789bd980dabe6cbf8e8cef9df248e30d6c3d
Sahil-Mishra63 <sahilmishrayt63@gmail.com>
2025-10-19 21:36:15 +0530

    Some minore changes

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit, OnInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { gsap } from 'gsap';
 import { CommonModule } from '@angular/common';
 import { NgForm } from '@angular/forms';
@@ -14,6 +14,8 @@ import { FormsModule } from '@angular/forms';
 })
 export class LandingComponent implements AfterViewInit {
 
+  @ViewChild('contactFormElement') contactFormElement!: ElementRef<HTMLFormElement>;
+
   ngAfterViewInit(){
       gsap.from(".hero-title", {y:30, opacity:0, duration:0.8});
       gsap.from(".hero-sub", { y: 15, opacity: 0, duration: 0.8, delay: 0.15 });
@@ -21,12 +23,53 @@ export class LandingComponent implements AfterViewInit {
       const btn = document.querySelector('.cta-btn')!;
       const popup = document.getElementById('contactPopup')!;
 
-      btn.addEventListener('click', () => {
-        popup.style.display = 'block';
-      });
+      // btn.addEventListener('click', () => {
+      //   popup.style.display = 'block';
+      // });
+  }
+
+  openPopup(){
+    const popup =  document.getElementById('contactPopup')!;
+    popup.style.display = 'block';
+  }
+
+  closePopup(){
+    const popup =  document.getElementById('contactPopup')!;
+    popup.style.display = 'none';
   }
 
   email = 'pixelroot.business@gmail.com';
+
+  //Emailjs setup
+  sendEmail(form: NgForm){
+    if(form.invalid) return;
+
+    emailjs.sendForm(
+      'service_6j7zb6q',
+      'template_p0390rf',
+      this.contactFormElement.nativeElement,
+      'IpUHwSzF-bYyRcWZI'
+    ).then(
+      (result: EmailJSResponseStatus) => {
+        console.log(result.text);
+        alert("Project request sent! We will contact you soon.");
+        form.reset();
+      },
+      (error) => {
+        console.error('FAILED...', error);
+        alert('Oops, something went wrong...');
+      }
+    )
+  }
+ 
+
+  scrollTo(event: Event, sectionId: string) {
+  event.preventDefault(); // prevent default jump
+  const el = document.getElementById(sectionId);
+  if (el) {
+    el.scrollIntoView({ behavior: 'smooth' });
+  }
+}
 
   projects = [
   {
@@ -64,40 +107,7 @@ export class LandingComponent implements AfterViewInit {
 ];
 
   
-  //Emailjs setup
-  sendEmail(form: NgForm){
-    if(form.invalid) return;
-
-    emailjs.sendForm(
-      'service_6j7zb6q',
-      'template_p0390rf',
-      form.form as unknown as HTMLFormElement,
-      'IpUHwSzF-bYyRcWZI'
-    ).then(
-      (result: EmailJSResponseStatus) => {
-        console.log(result.text);
-        alert("Project request sent! We will contact you soon.");
-        form.reset();
-      },
-      (error) => {
-        console.error('FAILED...', error);
-        alert('Oops, something went wrong...');
-      }
-    )
-  }
- 
-  closePopup() {
-      const popup = document.getElementById('contactPopup')!;
-      popup.style.display = 'none';
-  }
-
-  scrollTo(event: Event, sectionId: string) {
-  event.preventDefault(); // prevent default jump
-  const el = document.getElementById(sectionId);
-  if (el) {
-    el.scrollIntoView({ behavior: 'smooth' });
-  }
-}
+  
 
 
 }
